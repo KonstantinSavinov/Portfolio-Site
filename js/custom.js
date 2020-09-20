c2b3663775694a94158e4dab4687ecd4e433afee
KonstantinSavinov <kostyasavinov96@gmail.com>
2020-09-19 20:11:02 -0400

    First commit

---
+++ js/custom.js
@@ -0,0 +1,62 @@
+(function($) {
+
+	"use strict";
+
+	$(document).ready(function() {
+
+		$(".revealator-delay1").addClass('no-transform');
+
+
+		if ($('.grid').length) {
+			new CBPGridGallery( document.getElementById( 'grid-gallery' ) );
+		}
+
+		function checkSize() {
+			if ($( document ).width() > 992) {
+				var btn_hover = "";
+				$(".btn").each(function() {
+					var btn_text = $(this).text();
+					$(this).addClass(btn_hover).empty().append("<span data-hover='" + btn_text + "'>" + btn_text + "</span>");
+				});
+			}
+		}
+		checkSize();
+		window.addEventListener('resize', function () {
+			checkSize();
+		});
+
+		var item = $(".grid li figure");
+		var elementsLength = item.length;
+		for (var i = 0; i < elementsLength; i++) {
+			$(item[i]).hoverdir();
+		}
+
+		$(".contactform").on("submit", function() {
+			$(".output_message").text("Sending...");
+
+			var form = $(this);
+			$.ajax({
+				url: form.attr("action"),
+				method: form.attr("method"),
+				data: form.serialize(),
+				success: function(result) {
+					if (result == "success") {
+						$(".form-inputs").css("display", "none");
+						$(".box p").css("display", "none");
+						$(".contactform").find(".output_message").addClass("success");
+						$(".output_message").text("Message Sent!");
+					} else {
+						$(".tabs-container").css("height", "440px");
+
+						$(".contactform").find(".output_message").addClass("error");
+						$(".output_message").text("Error Sending!");
+					}
+				}
+			});
+
+			return false;
+		});
+
+	});
+
+})(jQuery);
